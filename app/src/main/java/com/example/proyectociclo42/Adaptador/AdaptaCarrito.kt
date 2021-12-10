60356191e6ab6f83f006e8fd1181ce83e6f45261
jhona <jhona@DESKTOP-HRC1764>
2021-12-09 22:01:59 -0500

    Función de eliminar registros

--- app/src/main/java/com/example/proyectociclo42/Adaptador/AdaptaCarrito.kt
+++ app/src/main/java/com/example/proyectociclo42/Adaptador/AdaptaCarrito.kt
@@ -3,6 +3,7 @@ package com.example.proyectociclo42.Adaptador
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -10,7 +11,7 @@ import com.example.proyectociclo42.R
 import com.example.proyectociclo42.model.ProductosCarrito
 import com.squareup.picasso.Picasso
 
-class AdaptaCarrito (private val dataSet: MutableList<ProductosCarrito>): RecyclerView.Adapter<AdaptaCarrito.ViewHolder>(){
+class AdaptaCarrito (private val dataSet: MutableList<ProductosCarrito>, private val listener: OnItemClickListener): RecyclerView.Adapter<AdaptaCarrito.ViewHolder>(){
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_car, parent, false)
@@ -33,12 +34,27 @@ class AdaptaCarrito (private val dataSet: MutableList<ProductosCarrito>): Recycl
         return dataSet.size
     }
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val titleItems : TextView = itemView.findViewById<TextView>(R.id.titleItem)
-        val costItems :TextView = itemView.findViewById<TextView>(R.id.costItem)
-        val imagenItem : ImageView = itemView.findViewById<ImageView>(R.id.imagenItem)
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+        val titleItems: TextView = itemView.findViewById<TextView>(R.id.titleItem)
+        val costItems: TextView = itemView.findViewById<TextView>(R.id.costItem)
+        val imagenItem: ImageView = itemView.findViewById<ImageView>(R.id.imagenItem)
+        val delete: Button = itemView.findViewById<Button>(R.id.delete)
+
+        init {
+            delete.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            if (adapterPosition != RecyclerView.NO_POSITION) {
+                listener.onItemClick(adapterPosition)
+            }
+        }
+    }
+
+    interface OnItemClickListener{
+        fun onItemClick(position: Int)
+    }
 
 }
 
 
-}
